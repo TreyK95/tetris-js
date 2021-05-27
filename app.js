6a9d1de5ff093ebae72c800fc0b9d802125feed2
Trey Kemp <kemptrey@outlook.com>
2021-05-27 09:18:22 -0600

    app.js set up

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector("gird");
+  let squares = Array.from(document.querySelectorAll(".grid div"));
+  const ScoreDisplay = document.querySelector("#score");
+  const StartBtn = document.querySelector("#start-button");
+  const width = 10;
+});

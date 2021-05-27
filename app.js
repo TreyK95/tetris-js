6302ad2ab5e4588c5b07228223ecae864f39b6ba
Trey Kemp <kemptrey@outlook.com>
2021-05-27 10:21:41 -0600

    freeze fuction working

--- app.js
+++ app.js
@@ -75,5 +75,24 @@ document.addEventListener("DOMContentLoaded", () => {
     undraw();
     currentPosition += width;
     draw();
+    freeze();
+  }
+
+  //freeze function
+  function freeze() {
+    if (
+      current.some((index) =>
+        squares[currentPosition + index + width].classList.contains("taken")
+      )
+    ) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add("taken")
+      );
+      //start a new tatromino falling
+      random = Math.floor(Math.random() * theTetrominoes.length);
+      current = theTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
   }
 });
